a488f4ba717817f953905f0dc5f90a7c1db21056
rocket7642 <Connor.leonie@gmail.com>
2026-06-28 19:06:35 -0700

    Removed hard lock building enforcement, increased weights to favor action instead.

--- Main/Connection/GameMessager.py
+++ Main/Connection/GameMessager.py
@@ -456,20 +456,20 @@ def receive_messages(conn, addr):
 					state.previous_positions[unit['id']] = (unit['x'], unit['z'])
 
 					# Bypass weight system, prevent it from doing something while building
-					if unit['is_constructing'] == 1:
-						action = config.NOOP_ACTION
-						best_target = (map_utils.normalize_x(unit['x']), map_utils.normalize_z(unit['z']))
-						best_score = 0.0
-						best_candidate_kind = 'noop'
-						discrete_action = config.ACTION_BUILD
-					else:
-						action, best_target, best_score, best_candidate_kind, discrete_action = agent_core.get_action(
-							state_vec,
-							unit['x'],
-							unit['z'],
-							unit['y'],
-							unit['id']
-						)
+					# if unit['is_constructing'] == 1:
+					# 	action = config.NOOP_ACTION
+					# 	best_target = (map_utils.normalize_x(unit['x']), map_utils.normalize_z(unit['z']))
+					# 	best_score = 0.0
+					# 	best_candidate_kind = 'noop'
+					# 	discrete_action = config.ACTION_BUILD
+					# else:
+					action, best_target, best_score, best_candidate_kind, discrete_action = agent_core.get_action(
+						state_vec,
+						unit['x'],
+						unit['z'],
+						unit['y'],
+						unit['id']
+					)
 
 					# Store chosen discrete action (Move vs Build) for training
 					state.previous_discrete_actions[unit['id']] = discrete_action
